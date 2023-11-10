c44960c4306b9d0aaf946e4cd747d38bf7e998af
AyushSharma-pdf <ayusharmamta@gmail.com>
2023-11-11 01:41:21 +0530

    Made LatestArrivals page working

--- src/pages/LatestArrivals.js
+++ src/pages/LatestArrivals.js
@@ -13,47 +13,57 @@ function LatestArrivals(props) {
     return (
         <>
             <div className="container my-5">
-                <Row md={2} xs={1} lg={3} className="g-3">
+                <Row md={2} xs={1} lg={3} className="gx-3 gy-4">
                     {fictionItems.map((item) => (
                         <Col key={item.id}>
                             <StoreItem {...item} mode={props.mode} />
                         </Col>
                     ))}
                 </Row>
+
                 <div className="latest-arrivals-card-gap my-4"></div>
-                <Row md={2} xs={1} lg={3} className="g-3">
+
+                <Row md={2} xs={1} lg={3} className="gx-3 gy-4">
                     {nonFictionItems.map((item) => (
                         <Col key={item.id}>
                             <StoreItem {...item} mode={props.mode} />
                         </Col>
                     ))}
                 </Row>
+
                 <div className="latest-arrivals-card-gap my-4"></div>
-                <Row md={2} xs={1} lg={3} className="g-3">
+
+                <Row md={2} xs={1} lg={3} className="gx-3 gy-4">
                     {biographyItems.map((item) => (
                         <Col key={item.id}>
                             <StoreItem {...item} mode={props.mode} />
                         </Col>
                     ))}
                 </Row>
+
                 <div className="latest-arrivals-card-gap my-4"></div>
-                <Row md={2} xs={1} lg={3} className="g-3">
+
+                <Row md={2} xs={1} lg={3} className="gx-3 gy-4">
                     {educationItems.map((item) => (
                         <Col key={item.id}>
                             <StoreItem {...item} mode={props.mode} />
                         </Col>
                     ))}
                 </Row>
+
                 <div className="latest-arrivals-card-gap my-4"></div>
-                <Row md={2} xs={1} lg={3} className="g-3">
+
+                <Row md={2} xs={1} lg={3} className="gx-3 gy-4">
                     {financeItems.map((item) => (
                         <Col key={item.id}>
                             <StoreItem {...item} mode={props.mode} />
                         </Col>
                     ))}
                 </Row>
+
                 <div className="latest-arrivals-card-gap my-4"></div>
-                <Row md={2} xs={1} lg={3} className="g-3">
+
+                <Row md={2} xs={1} lg={3} className="gx-3 gy-4">
                     {selfHelpItems.map((item) => (
                         <Col key={item.id}>
                             <StoreItem {...item} mode={props.mode} />
